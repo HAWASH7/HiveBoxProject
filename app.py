3abec704129f18eb56ca9e4744b91052b19eea9a
Mohammad <mahawash22@gmail.com>
2024-10-21 12:37:59 +0300

    Completed Phase 2.3: Added Dockerfile and ran app locally

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def add_sensor_data():
     return render_template('add_sensor_data.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
